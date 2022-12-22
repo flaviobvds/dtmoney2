50cc1c67949d985ede5cb62589a061e88c79baf1
flaviobvds <40229603+flaviobvds@users.noreply.github.com>
2022-12-22 16:15:04 -0400

    styled 2

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export function App() {
   return (
     <div className="App">
-     <h1>Hello dtmoney2!</h1>
+     <h1>Hello dtmoney2! a</h1>
     </div>
   );
 }
