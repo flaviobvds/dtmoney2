685f844c36e835a320dfcbdf41d540b4b7fe56bb
flaviobvds <40229603+flaviobvds@users.noreply.github.com>
2022-12-22 17:07:17 -0400

    global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import styled from 'styled-components'
+import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
     <div className="App">
-     <h1>Hello dtmoney2! a</h1>
+     <h1>Hello World!</h1>
+     <GlobalStyle />
     </div>
   );
 }
