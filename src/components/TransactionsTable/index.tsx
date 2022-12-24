8f431271d7967f321c1eb13dfc6c7f1808e9759b
flaviobvds <40229603+flaviobvds@users.noreply.github.com>
2022-12-24 16:10:46 -0400

    modal finished

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,15 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Container } from "./styles";
 
+interface Transaction {
+    id: number;
+    title: string;
+    value: number;
+    type: string;
+    category: string;
+    createdAt: string;
+}
+
 export function TransactionsTable() {
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
+    
     useEffect(() => {
         api.get('http://localhost:3000/api/transactions')
-        .then(response => console.log(response.data))
-    }, []);
-    console.log('transactions table rodando');
-    
-    
+        .then(response => setTransactions(response.data.transactions))
+    }, []); 
     
     return( 
         <Container>
@@ -24,19 +32,26 @@ export function TransactionsTable() {
                 </thead>
 
                 <tbody>
-                    <tr>
-                        <td className="title">Desenvolvimento de Website</td>
-                        <td className="deposit">R$ 12.000,00</td>
-                        <td>Desenvolvimento</td>
-                        <td>20/02/2021</td>
-                    </tr>
-
-                    <tr>
-                        <td className="title">Aluguel</td>
-                        <td className="withdraw">- R$ 1.100,00</td>
-                        <td>Casa</td>
-                        <td>18/02/2021</td>
-                    </tr>
+                    
+                    {transactions.map(transaction => {
+                        return (
+                            <tr key={transaction.id}>
+                                <td className="title">{transaction.title}</td>
+                                <td className={transaction.type}>
+                                    {new Intl.NumberFormat('pt-BR', {
+                                        style: 'currency',
+                                        currency: 'BRL',
+                                    }).format(transaction.value)}
+                                </td>
+                                <td>{transaction.category}</td>
+                                <td>
+                                    {new Intl.DateTimeFormat('pt-BR').format(
+                                        new Date(transaction.createdAt)
+                                    )}
+                                </td>
+                            </tr>
+                        );
+                    })}
                     
                 </tbody>
             </table>
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 createServer({
+    models: {
+        transaction: Model
+    },
+
+    seeds(server) {
+        server.db.loadData({
+            transactions: [
+                {
+                    id: 1,
+                    title: 'Freelance de website',
+                    value: 1500,
+                    type: 'deposit',
+                    category: 'Desenvolvimento',
+                    createdAt: new Date('2021-01-02 09:00:00'),
+                },
+                {
+                    id: 2,
+                    title: 'Aluguel',
+                    value: 1100,
+                    type: 'withdraw',
+                    category: 'Casa',
+                    createdAt: new Date('2021-01-18 19:00:00'),
+                }
+            ]
+        })
+    },
+    
     routes() {
         this.namespace = 'api'
 
         this.get('/transactions', () => {
-            return [
-                {
-                    id: 1,
-                    title: 'Transaction 1',
-                    amount: 400,
-                    type: 'deposit',
-                    category: 'Food',
-                    createdAt: new Date()
-                }
-            ]
+            return this.schema.all('transaction');
+        })
+
+        this.post('/transactions', (schema, request) => {
+            const data = JSON.parse(request.requestBody);
+            return schema.create('transaction',data);
         })
     }
 })
